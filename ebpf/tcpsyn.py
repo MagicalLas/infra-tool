f9d0aaf6b5273db110ff26b99a4a7ecb379f6249
Las <haho013039@gmail.com>
2023-10-08 01:34:10 +0900

    Update tcpsyn.py

--- ebpf/tcpsyn.py
+++ ebpf/tcpsyn.py
@@ -1,45 +1,81 @@
-from bcc import BPF
-import time
+#!/usr/bin/env python
+# @lint-avoid-python-3-compatibility-imports
+#
+# tcpsynbl      Show TCP SYN backlog.
+#               For Linux, uses BCC, eBPF. Embedded C.
+#
+# USAGE: tcpsynbl [-4 | -6] [-h]
+#
+# Copyright (c) 2019 Brendan Gregg.
+# Licensed under the Apache License, Version 2.0 (the "License").
+# This was originally created for the BPF Performance Tools book
+# published by Addison Wesley. ISBN-13: 9780136554820
+# When copying or porting, include this comment.
+#
+# 03-Jul-2019   Brendan Gregg   Ported from bpftrace to BCC.
 
-# eBPF 프로그램 정의
-program = """
-#include <uapi/linux/ptrace.h>
+from __future__ import print_function
+import argparse
+from bcc import BPF
+from time import sleep
+from struct import pack
+from socket import inet_ntop, AF_INET, AF_INET6
+
+# load BPF program
+bpf_text = """
 #include <net/sock.h>
-#include <bcc/proto.h>
 
 typedef struct backlog_key {
-    u32 pid;
     char task[TASK_COMM_LEN];
+    u32 pid;
+    u32 saddr;
+    u32 daddr;
+    u16 lport;
+    u16 dport;
 } backlog_key_t;
 
-BPF_HASH(dist, backlog_key_t);
+BPF_HISTOGRAM(dist, backlog_key_t);
 
 int do_entry(struct pt_regs *ctx) {
-    struct sock *sk = (struct sock *)PT_REGS_PARM1(ctx);
+    // get pid
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
 
+    struct sock *sk = (struct sock *)PT_REGS_PARM2(ctx);
+
+    // get details
+    u16 family = 0, lport = 0, dport;
+
+    // build datas
     backlog_key_t key = {};
-    u64 pid_tgid = bpf_get_current_pid_tgid();
-    u32 pid = pid_tgid >> 32;
+    key.saddr = sk->__sk_common.skc_rcv_saddr;
+    key.daddr = sk->__sk_common.skc_daddr;
+    key.lport = sk->__sk_common.skc_num;
+    key.dport = sk->__sk_common.skc_dport;
     key.pid = pid;
 
     bpf_get_current_comm(&key.task, sizeof(key.task));
 
-    dist.increment(key);
+    // add data
+    dist.increment(key, 1);
 
     return 0;
 };
+
 """
 
-b = BPF(text=program)
+b = BPF(text=bpf_text)
 
-# `tcp_v4_connect` 함수에 eBPF 프로그램을 연결
-b.attach_kprobe(event="inet_csk_accept", fn_name="trace_connect")
+b.attach_kprobe(event="reqsk_alloc", fn_name="do_entry")
 
-print("Tracing tcp_v4_connect... Ctrl-C to end.")
+print("Tracing SYN backlog size. Ctrl-C to end.")
+
 try:
-    while True:
-        time.sleep(1)  # 10초마다 카운터 값을 출력
-        for key, value in b["dist"].items():
-            print(f"tcp_v4_connect calls: {value.value}")
+    sleep(99999999)
 except KeyboardInterrupt:
-    pass
+    print()
+
+dist = b.get_table("dist")
+for k, v in dist.items():
+    saddr = inet_ntop(AF_INET, pack("I",k.saddr))
+    daddr = inet_ntop(AF_INET, pack("I",k.daddr))
+    print([f"{k.pid}+{k.task}({saddr}:{k.lport}->{daddr}:{k.dport})", v.value])
